39db000d9395b54054ef5a88fec12e4232b96137
J. Allen Burton <jburton6@my.westga.edu>
2020-01-17 14:19:40 -0500

    Fixed error messages to display specific information beneath each form field.

--- display_savings.php
+++ display_savings.php
@@ -1,43 +1,49 @@
 <?php
 // get the data from the form
 $starting_balance = filter_input ( INPUT_POST, 'starting_balance', FILTER_VALIDATE_FLOAT );
-$monthly_contributions = filter_input ( INPUT_POST, 'monthly_contributions', FILTER_VALIDATE_FLOAT );
+$monthly_contribution = filter_input ( INPUT_POST, 'monthly_contributions', FILTER_VALIDATE_FLOAT );
 $years = filter_input ( INPUT_POST, 'years', FILTER_VALIDATE_INT );
 $interest_rate = filter_input ( INPUT_POST, 'interest_rate', FILTER_VALIDATE_FLOAT );
 
-// validate inputs
-if ($starting_balance == FALSE) {
-	$error_message = 'The starting balance must be a valid number.';
+// validate inputs and set up error messages
+$error_message_balance = '';
+if ($starting_balance === FALSE) {
+	$error_message_balance = 'The starting balance must be a valid number.';
 } else if ($starting_balance < 0) {
-	$error_message = 'The starting balance must be greater than or equal to zero.';
-} else if ($monthly_contributions == FALSE) {
-	$error_message = 'The monthly contribution must be a valid number';
-} else if ($monthly_contributions < 0) {
-	$error_message = 'The monthly contribution must be greater than or equal to zero';
-} else if ($years == FALSE) {
-	$error_message = 'The time to grow in years must be a valid whole number.';
+	$error_message_balance = 'The starting balance must be greater than or equal to zero.';
+} 
+
+
+$error_message_contribution = '';
+if ($monthly_contribution === FALSE) {
+	$error_message_contribution = 'The monthly contribution must be a valid number';
+} else if ($monthly_contribution < 0) {
+	$error_message_contribution = 'The monthly contribution must be greater than or equal to zero';
+} 
+
+$error_message_years = '';
+if ($years === FALSE) {
+	$error_message_years = 'The time to grow in years must be a valid whole number.';
 } else if ($years <= 0) {
-	$error_message = 'The time to grow must be a number of years greater than zero.';
-} else if ($interest_rate == FALSE) {
-	$error_message = 'The interest rate must be a valid number.';
+	$error_message_years = 'The time to grow must be a number of years greater than zero.';
+}
+
+$error_message_rate = '';
+if ($interest_rate === FALSE) {
+	$error_message_rate = 'The interest rate must be a valid number.';
 } else if ($interest_rate <= 0) {
-	$error_message = 'The interest rate must be greater than zero.';
-} else {
-	$error_message = '';
+	$error_message_rate = 'The interest rate must be greater than zero.';
 }
 
 // if an error is present return to index page with error message
-if ($error_message != '') {
-	include (index.php);
-	exit();
+if ($error_message_balance != '' || $error_message_contribution != '' || $error_message_years != '' || $error_message_rate != '') {
+	include ('index.php');
+	exit ();
 }
 
-// apply currency formatting to the dollar and percent amounts
-$starting_balance_f = "$" . number_format ( $starting_balance, 2 );
-$monthly_contributions_f = "$" . number_format ( $monthly_contributions, 2 );
-$interest_rate_f = $interest_rate . "%";
 
-
+//initialize current value
+$current_value = $starting_balance;
 
 ?>
 <!DOCTYPE html>
@@ -51,7 +57,31 @@ $interest_rate_f = $interest_rate . "%";
 <body>
 	<main>
 		<h1>Annual Savings</h1>
-
+		<table>
+			<thead>
+				<tr>
+					<th>Year</th>
+					<th>Savings</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php for ($year_number = 1; $year_number <= $years; $year_number++) {;?>
+				<tr>
+					<td>
+						<?php echo $year_number; ?>
+					</td>
+					<td>
+						<?php 
+						for ($month_number = 0; $month_number < 12; $month_number++) {
+							$current_value = ($current_value + $monthly_contribution) * (1 + (($interest_rate * 0.01) / 12));
+						}
+						echo "$" . number_format ( $current_value, 2 );
+						?>
+					</td>
+				</tr>
+			<?php } ?>
+			</tbody>
+		</table>
 
 	</main>
 </body>
--- index.php
+++ index.php
@@ -1,54 +1,89 @@
 <?php
-if (! isset ( $starting_balance )) {
-	$starting_balance = '';
+if (!isset ($starting_balance)) {
+    $starting_balance = '';
 }
-if (! isset ( $monthly_contributions )) {
-	$monthly_contributions = '';
+if (!isset ($monthly_contribution)) {
+    $monthly_contribution = '';
 }
-if (! isset ( $years )) {
-	$years = '';
+if (!isset ($years)) {
+    $years = '';
 }
-if (! isset ( $interest_rate )) {
-	$interest_rate = '';
+if (!isset ($interest_rate)) {
+    $interest_rate = '';
 }
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<title>Savings Calculator</title>
-<link rel="stylesheet" type="text/css" href="main.css">
+    <title>Savings Calculator</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
 <body>
-	<main>
-		<h1>Savings Calculator</h1>
-		<?php if (!empty($error_message)) {?>
-		<div id="error_message">
-			<p class="error"><?php echo $error_message; ?></p>
-		</div>
-		<?php }?>
-		<form action="display_savings.php" method="post">
-			<div id="data">
-				<label>Starting balance:</label> <input type="text"
-					name="starting_balance"
-					value="<?php echo htmlspecialchars($starting_balance); ?>"> <br> <label>Monthly
-					contributions:</label> <input type="text"
-					name="monthly_contributions"
-					value="<?php echo htmlspecialchars($monthly_contributions); ?>"> <br>
+<main>
+    <h1>Savings Calculator</h1>
+    <?php if (!empty($error_message)) { ?>
+        <div id="error_message">
+            <p class="error"><?php echo $error_message; ?></p>
+        </div>
+    <?php } ?>
+    <form action="display_savings.php" method="post">
+        <div id="data">
+            <label>Starting balance:</label> 
+            <input type="text"
+                   name="starting_balance"
+                   value="<?php echo htmlspecialchars($starting_balance); ?>">
+            <br> 
+            <?php if (!empty($error_message_balance)) { ?>
+        		<div id="error_message">
+            	<span class="error"><?php echo $error_message_balance; ?></span>
+        		</div>
+    			<?php } ?>
+            <br>
+            
+            <label>Monthly contributions:</label> 
+            <input type="text"
+                   name="monthly_contributions"
+                   value="<?php echo htmlspecialchars($monthly_contribution); ?>"> 
+            <br>
+            <?php if (!empty($error_message_contribution)) { ?>
+        		<div id="error_message">
+            	<span class="error"><?php echo $error_message_contribution; ?></span>
+        		</div>
+    			<?php } ?>
+            <br>
 
-				<label>Time to grow in years:</label> <input type="text"
-					name="years" value="<?php echo htmlspecialchars($years); ?>"> <br>
-				<label>Annual interest rate (interest is compunded monthly):</label>
-				<input type="text" name="interest_rate"
-					value="<?php echo htmlspecialchars($interest_rate); ?>"> <br>
-			</div>
+            <label>Time to grow in years:</label> 
+            <input type="text"
+                   name="years" 
+                   value="<?php echo htmlspecialchars($years); ?>">
+            <br>
+            <?php if (!empty($error_message_years)) { ?>
+        		<div id="error_message">
+            	<span class="error"><?php echo $error_message_years; ?></span>
+        		</div>
+    			<?php } ?>
+            <br>
+            
+            <label>Annual interest rate (interest is compunded monthly):</label>
+            <input type="text" 
+            	   name="interest_rate"
+                   value="<?php echo htmlspecialchars($interest_rate); ?>"> <br>
+			<?php if (!empty($error_message_rate)) { ?>
+        		<div id="error_message">
+            	<span class="error"><?php echo $error_message_rate; ?></span>
+        		</div>
+    			<?php } ?>
+            <br>
+        </div>
 
-			<div id="buttons">
-				<label>&nbsp;</label> <input type="submit" value="Calculate"><br>
-			</div>
+        <div id="buttons">
+            <label>&nbsp;</label> <input type="submit" value="Calculate"><br>
+        </div>
 
-		</form>
-	</main>
+    </form>
+</main>
 </body>
 </html>
